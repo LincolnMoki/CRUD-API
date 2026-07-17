5caad8492a8196805898c6028d401b09907e6e65
Lincoln Moki <mwithiamoki@gmail.com>
2026-07-17 14:14:15 +0300

    hello server

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
 # main.py
 from fastapi import FastAPI
 
-
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return {
-        "name": "Task API",
-        "Version": "1.0.0",
-        "endpoint": ["/tasks"]
-    }   
-@app.get("/health")
-def read_health():
-    return {"status": "ok"}
+def home():
+    return {"message": "Hello, World!"}
