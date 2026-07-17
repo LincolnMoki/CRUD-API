d58fa98533f99d6b047d5cb6b7f41aa6406c5d11
Lincoln Moki <mwithiamoki@gmail.com>
2026-07-17 14:08:49 +0300

    root and health endpoints

--- main.py
+++ main.py
@@ -5,5 +5,12 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def home():
-    return {"message": "Hello, World!"}                 
+def read_root():
+    return {
+        "name": "Task API",
+        "Version": "1.0.0",
+        "endpoint": ["/tasks"]
+    }   
+@app.get("/health")
+def read_health():
+    return {"status": "ok"}
