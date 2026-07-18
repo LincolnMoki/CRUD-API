d158b5bc3ac108073f681d3a26ce1381db018afd
Lincoln Moki <mwithiamoki@gmail.com>
2026-07-18 22:28:23 +0300

    Full CRUD

--- main.py
+++ main.py
@@ -12,7 +12,10 @@ tasks = [
 
 class TaskCreate(BaseModel):
     title: str
-    done: bool
+
+class TaskUpdate(BaseModel):
+    title: str | None = None
+    done: bool | None = None
 
 @app.get("/")
 def read_root():
@@ -47,3 +50,24 @@ def create_task(task: TaskCreate):
     new_task = {"id": new_id, "title":task.title,"done":False}
     tasks.append(new_task)
     return new_task
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, update: TaskUpdate):
+    if not update.title and update.title.strip():
+        raise HTTPException(status_code=400, detail="Title is required")
+    
+    for task in tasks:
+        if task["id"] == task_id:
+            task["title"] == update.title
+            task["done"] == update.done
+            return task
+        
+    raise HTTPException(status_code=404, detail=f"Task {task_id} not found")
+
+@app.delete("/tasks/{task_id}", status_code=204)
+def delete_task(task_id: int):
+    for i, task in enumerate(tasks):
+        if task["id"] == task_id:
+            tasks.pop(i)
+            return
+    raise HTTPException(status_code=404, detail=f"Task {task_id} not found")
